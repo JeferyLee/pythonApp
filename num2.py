2095f3f8d31bf96dffb40238a7a2b52277e5d07c
JeferyLee <40261147+JeferyLee@users.noreply.github.com>
2018-08-27 16:41:46 +0800

    Add files via upload

--- num2.py
+++ num2.py
@@ -51,9 +51,9 @@ names=np.array(['Bob','Joe','Will','Bob','Will','Joe','Joe'])
 # 满足正太分布的7*4随机数
 data=np.random.randn(7,4)
 # print(names)
-print(data)
-print(names=='Bob')
-print(data[names=='Bob'])
+# print(data)
+# print(names=='Bob')
+# print(data[names=='Bob'])
 # 打印出数组中对应的索引
 
 # [ True False False  True False False False]
@@ -76,6 +76,38 @@ print(data[names=='Bob'])
 
 # 将data中所有负值都赋值为0.
 data[data<0]=0
-print(data)
+# print(data)
 
 # 花式索引
+arr=np.empty((8,4))
+print('----------------')
+for i in range(8):
+    arr[i]=i
+print(arr[[1]])
+# 一次传入多个索引数组会有一点特别。它返回的是一个一维数组，其中的元素对应各个索引元组
+# arr=np.arange(32).reshape(8,4)
+# print(arr)
+# print('---------')
+# print(arr[[1, 5, 7, 2], [0, 3, 1, 2]])
+
+# 数组转置和轴对换
+arr=np.arange(15).reshape(3,5)
+print(arr)
+print(arr.T)
+arr=np.random.randn(6,3)
+
+# 4.2 通用函数：快速的元素级数组函数
+# 通用函数（即ufunc）是一种对ndarray中的数据执行元素级运算的函数。你可以将其看做简单函数
+# （接受一个或多个标量值，并产生一个或多个标量值）的矢量化包装器。
+
+# arr=np.arange(10)
+# print(arr)
+# print(np.sqrt(arr))
+
+arr=np.random.randn(7)*5
+print(arr)
+remainder,whole_part=np.modf(arr)
+print('abc')
+print(remainder)
+print(whole_part)   #modf()函数返回数组的小数和整数部分。
+
